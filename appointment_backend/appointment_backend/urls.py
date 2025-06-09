5e9a1d881740cd956a264369d3f1ffb0551a3bc2
siddhesh <siddheshneharkar70@gmail.com>
2025-06-09 16:40:20 +0530

    Add simple root endpoint for home

--- appointment_backend/appointment_backend/urls.py
+++ appointment_backend/appointment_backend/urls.py
@@ -1,9 +1,14 @@
-# project_name/urls.py
+# appointment_backend/appointment_backend/urls.py
 
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Welcome to Medical Appointment Backend API")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('core.urls')), 
+    path('api/', include('core.urls')),
+    path('', home),  # <-- Add this line for root route
 ]
